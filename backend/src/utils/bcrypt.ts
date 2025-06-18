0650a5f27c69957cf0a621dc889acfbcc895d7f3
amirgolp <44730575+amirgolp@users.noreply.github.com>
2025-06-18 21:53:08 +0200

    updated

--- backend/src/utils/bcrypt.ts
+++
@@ -1,7 +0,0 @@
-import bcrypt from "src/utils/bcrypt";
-
-export const hashValue = async (value: string, saltRounds: number = 10) =>
-  await bcrypt.hash(value, saltRounds);
-
-export const compareValue = async (value: string, hashedValue: string) =>
-  await bcrypt.compare(value, hashedValue);
